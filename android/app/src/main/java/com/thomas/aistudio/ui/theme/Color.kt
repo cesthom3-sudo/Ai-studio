de7628373253dd0b8b5a92962f680611050cd605
cesthom3-sudo <cesthom3@gmail.com>
2026-09-19 22:14:59 +0200

    Upload Thomas AI Studio project files

---
+++ android/app/src/main/java/com/thomas/aistudio/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.thomas.aistudio.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgDark = Color(0xFF0E0E12)
+val SurfaceDark = Color(0xFF17171D)
+val CardDark = Color(0xFF1F1F27)
+val AccentPurple = Color(0xFF7C5CFC)
+val AccentPink = Color(0xFFFF5CA2)
+val TextPrimary = Color(0xFFF5F5F7)
+val TextSecondary = Color(0xFFA0A0AC)
